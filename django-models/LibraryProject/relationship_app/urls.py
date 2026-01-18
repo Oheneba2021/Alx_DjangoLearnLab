7e973651da435d04e029d6e99e8ecd2ffe0f6f59
Godwin Boakye <ohenebayb@gmail.com>
2026-01-18 18:35:09 +0000

    roles readjsuted

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
         views.register,
         name="register",
     ),
-    path("admin-role/", admin_view, name="admin_view"),
-    path("librarian-role/", librarian_view, name="librarian_view"),
-    path("member-role/", member_view, name="member_view"),
+    path("admin-role/", views.admin_view, name="admin_view"),
+    path("librarian-role/", views.librarian_view, name="librarian_view"),
+    path("member-role/", views.member_view, name="member_view"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,8 @@ from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout
-
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 
 # from django.http import HttpResponse
 from django.views.generic.detail import DetailView
@@ -57,3 +58,30 @@ class RegisterView(CreateView):
     success_url = reverse_lazy("login")
     
 register = RegisterView.as_view()
+
+
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+
+
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
