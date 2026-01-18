5bc3428ed606524a88d8b3c5d98e9b22147cb325
Godwin Boakye <ohenebayb@gmail.com>
2026-01-18 18:01:54 +0000

    login and logout views changed to class based views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import RegisterView
 
 
 from .models import Book
@@ -9,8 +11,8 @@ from .views import list_books, LibraryDetailView
 urlpatterns = [
     path('libraries/<int:library_id>/', views.LibraryDetailView.as_view(), name='LibraryDetailView'),
     path('books/', views.list_books, name='list_books'),
-       path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
-    path("register/", views.register_view, name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("register/", views.RegisterView.as_view(), name="register"),
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Library
 from .models import Book
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 # from django.http import HttpResponse
 from django.views.generic.detail import DetailView
@@ -19,30 +21,36 @@ class LibraryDetailView(DetailView):
     context_object_name = 'library'
     pk_url_kwarg = 'library_id'
 
-def login_view(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect("home")  # change if you use a different landing page
-    else:
-        form = AuthenticationForm()
+# def login_view(request):
+#     if request.method == "POST":
+#         form = AuthenticationForm(request, data=request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             login(request, user)
+#             return redirect("home")  # change if you use a different landing page
+#     else:
+#         form = AuthenticationForm()
 
-    return render(request, "relationship_app/login.html", {"form": form})
+#     return render(request, "relationship_app/login.html", {"form": form})
 
-@login_required
-def logout_view(request):
-    logout(request)
-    return render(request, "relationship_app/logout.html")
+# @login_required
+# def logout_view(request):
+#     logout(request)
+#     return render(request, "relationship_app/logout.html")
 
-def register_view(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("login")
-    else:
-        form = UserCreationForm()
+# def register_view(request):
+#     if request.method == "POST":
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("login")
+#     else:
+#         form = UserCreationForm()
 
-    return render(request, "relationship_app/register.html", {"form": form})
+#     return render(request, "relationship_app/register.html", {"form": form})
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = "relationship_app/register.html"
+    success_url = reverse_lazy("login")
