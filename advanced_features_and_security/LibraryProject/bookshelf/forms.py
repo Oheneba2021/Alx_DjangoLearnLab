6cd4b813ec8d30cdd1597a21c0f0bd38a87a19c3
Godwin Boakye <ohenebayb@gmail.com>
2026-01-25 17:46:13 +0000

    forms.py added

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,6 +2,16 @@ from django import forms
 from .models import Book
 
 
+class ExampleForm(forms.Form):
+    """
+    ExampleForm demonstrates secure handling and validation
+    of user input using Django forms.
+    """
+
+    title = forms.CharField(max_length=200)
+    author = forms.CharField(max_length=100)
+
+
 class BookForm(forms.ModelForm):
     """
     BookForm is used to validate and sanitize user input
