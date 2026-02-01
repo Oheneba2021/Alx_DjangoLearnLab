8f42b284614c622a28dadfb1a6fdaba913303e99
Godwin Boakye <ohenebayb@gmail.com>
2026-02-01 17:10:43 +0000

    authentication added

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,13 +2,23 @@ from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics, viewsets
-
+from rest_framework.permissions import IsAuthenticated, SAFE_METHODS, BasePermission
 
 # Create your views here.
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+  # Allow unrestricted access for demonstration purposes
+    
+class IsAdminUser(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
+    
+    
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
