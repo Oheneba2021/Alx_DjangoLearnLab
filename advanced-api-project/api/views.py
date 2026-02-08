7de8a7ea8a0c9252442e8ce22b7c62afd2d36efa
Godwin Boakye <ohenebayb@gmail.com>
2026-02-08 18:43:36 +0000

    filters added

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,11 +1,12 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
 
 from .models import Book
 from .serializers import BookSerializer
 from .filters import BookFilter
+
 
 
 class BookListView(generics.ListAPIView):
@@ -28,16 +29,9 @@ class BookListView(generics.ListAPIView):
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    # The three query features:
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-
-    # Filtering
+    filter_backends = [filters.DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = BookFilter
-
-    # Searching (supports related fields)
     search_fields = ["title", "author__name"]
-
-    # Ordering
     ordering_fields = ["id", "title", "publication_year"]
     ordering = ["id"]
 
