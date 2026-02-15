82b935fea04bcd62108a138bb9120b9268e07bde
Godwin Boakye <ohenebayb@gmail.com>
2026-02-15 17:03:23 +0000

    med]

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
     # Comments
     path("post/<int:pk>/comments/new/", views.CommentCreateView.as_view(), name="comment-create"),
-    path("comments/<int:pk>/edit/", views.CommentUpdateView.as_view(), name="comment-update"),
-    path("comments/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment-delete"),
+    path("comment/<int:pk>/edit/", views.CommentUpdateView.as_view(), name="comment-update"),
+    path("comment/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment-delete"),
 
 ]
